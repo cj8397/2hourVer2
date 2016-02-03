3be273c21311233ea390d0c6064e871738494099
hasssanhosseinpour <hassanhosseinpoor@yahoo.com>
2016-02-02 23:31:09 -0800

    adding role to all customer and admin pages in HomeController and changing dynamic menu.

--- 2Hours Ver2/2Hours Ver2/Controllers/HomeController.cs
+++ 2Hours Ver2/2Hours Ver2/Controllers/HomeController.cs
@@ -13,5 +13,132 @@ namespace _2Hours_Ver2.Controllers
         {
             return View();
         }
+
+        [Authorize(Roles = "Admin")]        
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminOrderDetail()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminOrders()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminOrderUpdate()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminProduct()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminProductAdd()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminProductDetail()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminProductUpdate()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminSupplier()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminSupplierAdd()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminSupplierDetail()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminSupplierUpdate()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminUser()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminUserAdd()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminUserDetail()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AdminUserUpdate()
+        {
+            return View();
+        }
+        // Admin and customer have permission to go to these pages.
+        [Authorize]
+        public ActionResult Profile_A()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult User_A()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult PasswordReset()
+        {
+            return View();
+        }
+
     }
 }
