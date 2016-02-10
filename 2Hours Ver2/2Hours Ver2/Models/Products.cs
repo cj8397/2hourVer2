1a079527d9d47f5a4aee825c5ccaf70a2002f71c
rosaliamatzumiya <rmg2694@gmail.com>
2016-02-10 14:45:43 -0800

    commented out products model to commit -rosalia

--- 2Hours Ver2/2Hours Ver2/Models/Products.cs
+++ 2Hours Ver2/2Hours Ver2/Models/Products.cs
@@ -11,32 +11,32 @@ namespace _2Hours_Ver2.Models
         public const string PRODUCT_NAME = "name";
         private mergedEntities db = new mergedEntities();
 
-        IEnumerable<ProductSearch> FilterProducts(IEnumerable<ProductSearch> products, string searchString)
-        {
-            // Filter results based on search.
-            if (!String.IsNullOrEmpty(searchString))
-                products = products.Where(
-                            p => p.ProductName.ToUpper().Contains(searchString.ToUpper()));
+        //IEnumerable<ProductSearch> FilterProducts(IEnumerable<ProductSearch> products, string searchString)
+        //{
+        //    // Filter results based on search.
+        //    if (!String.IsNullOrEmpty(searchString))
+        //        products = products.Where(
+        //                    p => p.ProductName.ToUpper().Contains(searchString.ToUpper()));
                             
-            return products;
-        }
+        //    return products;
+        //}
 
 
-        public IQueryable<ProductSearch> GetAllProducts(string searchString)
-        {
-            var products = (from p in db.Products
-                            from op in p.OrderProducts
-                            where op.productID == p.productID
-                            select new ProductSearch
-                            {
-                                ProductID = p.productID,
-                                ProductName = p.productName,
-                                Price = (decimal)p.price
-                            });
+        //public IQueryable<ProductSearch> GetAllProducts(string searchString)
+        //{
+        //    var products = (from p in db.Products
+        //                    from op in p.OrderProducts
+        //                    where op.productID == p.productID
+        //                    select new ProductSearch
+        //                    {
+        //                        ProductID = p.productID,
+        //                        ProductName = p.productName,
+        //                        Price = (decimal)p.price
+        //                    });
 
-            products = FilterProducts(products, searchString);
-            return products;
-        }
+        //    products = FilterProducts(products, searchString);
+        //    return products;
+        //}
 
     }//end product repo
 }
