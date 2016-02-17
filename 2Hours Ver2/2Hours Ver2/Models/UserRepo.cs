a77e46b246fd48ef89b6b74259f20fab413a42c1
parsa <f_lamee2002@yahoo.com>
2016-02-17 00:47:29 -0800

    create UpdateUser in UserRepo

---
+++ 2Hours Ver2/2Hours Ver2/Models/UserRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Hours_Ver2.Models
+{
+    class UserRepo
+    {
+
+        //  [HttpPost]
+        public AspNetUser UpdateUser(string id, string phone, string userName)
+        {
+
+            mergedEntities db = new mergedEntities();
+            AspNetUser aspNetUser = db.AspNetUsers.Where(a => a.Id == id)
+                            .FirstOrDefault();
+            aspNetUser.PhoneNumber = phone;
+            aspNetUser.UserName = userName;
+
+            db.SaveChanges();
+            return aspNetUser;
+        }
+    }
+}
