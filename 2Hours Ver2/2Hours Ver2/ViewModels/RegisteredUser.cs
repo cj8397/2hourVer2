372433322096fd578c57c8a370bef31b44157537
cheng <cheng.yi.jiang@gmail.com>
2016-02-17 16:42:28 -0800

    registered user - cheng

---
+++ 2Hours Ver2/2Hours Ver2/ViewModels/RegisteredUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Hours_Ver2.ViewModel
+{
+    public class RegisteredUser
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$",
+        ErrorMessage = "This is not a valid email address.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password Confirm")]
+        [Compare("Password", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string TelNumber { get; set; }
+
+        public string ConfirmLink { get; set; }
+
+        public RegisteredUser() { }
+
+        public RegisteredUser(string email, string userName, string confirmLink)
+        {
+            Email = email;
+            UserName = userName;
+            ConfirmLink = confirmLink;
+        }
+    }
+}
+
