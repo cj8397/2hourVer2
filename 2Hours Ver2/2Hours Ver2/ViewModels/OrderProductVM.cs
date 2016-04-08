c3e6500f0b42375d8cb1eec9e1f0975bdf15c8e4
hasssanhosseinpour <hassanhosseinpoor@yahoo.com>
2016-04-08 12:00:50 -0700

    last version

---
+++ 2Hours Ver2/2Hours Ver2/ViewModels/OrderProductVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _2Hours_Ver2.ViewModels
+{
+    public class OrderProductVM
+    {
+        [Required]
+        [Display(Name = "Order number")]
+        public int OrderNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Product Id")]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Display(Name = "Quantity")]
+        [Range(0, 10000, ErrorMessage = "Invalid Quantity!")]
+        public decimal Quantity { get; set; }
+    }
+}
